37231227a522023262c0af9ad2c8cd4a81f38263
Rakshitha R <109401667+RakshithaRShettigar@users.noreply.github.com>
2023-05-09 10:20:34 +0530

    Create swap_block_tb.v

---
+++ Blocking and Non-Blocking Part-1/Swapping using blocking/swap_block_tb.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+module swap_block_tb;
+
+	// Inputs
+	reg clk;
+
+	// Outputs
+	wire [3:0] a;
+	wire [3:0] b;
+
+	// Instantiate the Unit Under Test (UUT)
+	swap_block uut (
+		.a(a), 
+		.b(b), 
+		.clk(clk)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+
+	end
+	always #5 clk=~clk;
+      
+endmodule
+
