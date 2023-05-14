9247d1deceb991b46c04458a48a35ec5a96b67bf
Rakshitha R <109401667+RakshithaRShettigar@users.noreply.github.com>
2023-05-14 12:39:30 +0530

    Create shift_register_nonblocking.v

---
+++ Blocking and Non-Blocking Part-3/2 Bit Shift register using non blocking/shift_register_nonblocking.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+
+module shift_register_nonblocking(
+   input clk,
+	input [1:0] a,
+	output reg q1,
+	output reg q2
+    );
+	 integer i;
+	 //assign initial values
+	 initial 
+	 begin
+	 q1 = 0;
+	 q2 = 0;
+	 i = 0;
+	 end
+	 
+	 
+	always @(posedge clk)
+		 begin
+		  q1 <= a[i];
+		  q2 <= q1;
+		  i = i + 1;
+		 end
+
+
+endmodule
