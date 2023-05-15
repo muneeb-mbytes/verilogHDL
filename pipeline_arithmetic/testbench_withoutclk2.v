20eca38c8d8175b6e3c30aacc0e3f42518447ffb
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2023-05-15 16:48:46 +0530

    Updated  testbench_withoutclk2.v

--- pipeline_arithmetic/testbench_withoutclk2.v
+++ pipeline_arithmetic/testbench_withoutclk2.v
@@ -30,7 +30,7 @@ module tb;
 	reg [9:0] c;
 	reg [9:0] d;
 	reg clk1;
-		reg clk2;
+	reg clk2;
 	// Outputs
 	wire [9:0] F;
 
@@ -47,38 +47,25 @@ module tb;
 	);
 
 	initial 
-		// Initialize Inputs
-	//	begin
-		clk1 = 0;// clk2=0;
+		clk1 = 0;
 		always #5 clk1 = ~clk1;
-		/*repeat (20) // Generating two-phase clock
-begin
-#5 clk1 = 1; #5 clk1 = 0;
-#5 clk2 = 1; #5 clk2 = 0;
-end
-end	*/	
-		initial 
-			begin
-				#5 a = 10; b = 12; c = 6; d = 3; // 75 
-				#20 a = 10; b = 10; c = 5; d = 3; // 66 
-				#20 a = 20; b = 11; c = 1; d = 4; // 112 
-				#20 a = 10; b = 20; c = 5; d = 3; // 66
-				#20 a = 10; b = 10; c = 2; d = 4; // 49
-				#20 a = 15; b = 10; c = 8; d = 2; // 62
-				#20 a = 8; b = 15; c = 5; d = 0; // 0
-				#20 a = 30; b = 1; c = 2; d = 4; // 116
+	initial 
+		begin
+			#5 a = 10; b = 12; c = 6; d = 3; // 75 
+			#20 a = 10; b = 10; c = 5; d = 3; // 66 
+			#20 a = 20; b = 11; c = 1; d = 4; // 112 
+			#20 a = 10; b = 20; c = 5; d = 3; // 66
+			#20 a = 10; b = 10; c = 2; d = 4; // 49
+			#20 a = 15; b = 10; c = 8; d = 2; // 62
+			#20 a = 8; b = 15; c = 5; d = 0; // 0
+			#20 a = 30; b = 1; c = 2; d = 4; // 116
 			
-			end
-		initial begin
-		$monitor (" Time = %d , F= %d ",$time,F);
-	#180 $finish;
 		end
-		// Wait 100 ns for global reset to finish
-		
-        
-		// Add stimulus here
-
-	
-      
+	initial 
+		begin
+			$monitor (" Time = %d , F= %d ",$time,F);
+			#180 $finish;
+		end
+	   
 endmodule
 
