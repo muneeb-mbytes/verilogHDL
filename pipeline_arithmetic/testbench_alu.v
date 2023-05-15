9391b76aa364efed46231665311d197700c9f9f7
RAKSHITHA R NAYAK <73732585+rakshitharnayak@users.noreply.github.com>
2023-05-15 16:46:46 +0530

    Updated testbench_alu.v

--- pipeline_arithmetic/testbench_alu.v
+++ pipeline_arithmetic/testbench_alu.v
@@ -46,38 +46,35 @@ module testbench_alu;
 		.clk2(clk2)
 	);
 
-initial begin
+initial
+	begin
 		// Initialize Inputs
 		
 		clk1 = 0; clk2=0;
-		//always #10 clk = ~clk;
+		
 		repeat (20) // Generating two-phase clock
-begin
-#5 clk1 = 1; #5 clk1 = 0;
-#5 clk2 = 1; #5 clk2 = 0;
-end
-end		
-		initial 
 			begin
-				#5 a = 10; b = 12; c = 6; d = 3; // 75 
-				#20 a = 10; b = 10; c = 5; d = 3; // 66 
-				#20 a = 20; b = 11; c = 1; d = 4; // 112 
-				#20 a = 10; b = 20; c = 5; d = 3; // 66
-				#20 a = 10; b = 10; c = 2; d = 4; // 49
-				#20 a = 15; b = 10; c = 8; d = 2; // 62
-				#20 a = 8; b = 15; c = 5; d = 0; // 0
-				#20 a = 30; b = 1; c = 2; d = 4; // 116
-			
+    				#5 clk1 = 1; #5 clk1 = 0;
+				#5 clk2 = 1; #5 clk2 = 0;
 			end
-		initial begin
+	end		
+initial 
+	begin
+		#5 a = 10; b = 12; c = 6; d = 3; // 75 
+		#20 a = 10; b = 10; c = 5; d = 3; // 66 
+		#20 a = 20; b = 11; c = 1; d = 4; // 112 
+		#20 a = 10; b = 20; c = 5; d = 3; // 66
+		#20 a = 10; b = 10; c = 2; d = 4; // 49
+		#20 a = 15; b = 10; c = 8; d = 2; // 62
+		#20 a = 8; b = 15; c = 5; d = 0; // 0
+		#20 a = 30; b = 1; c = 2; d = 4; // 116
+			
+	end
+initial 
+	begin
 		$monitor (" Time = %d , F= %d ",$time,F);
 		#300 $finish;
-		end
-		// Wait 100 ns for global reset to finish
-		
-        
-		// Add stimulus here
-
-	      
+	end
+      
 endmodule
 
