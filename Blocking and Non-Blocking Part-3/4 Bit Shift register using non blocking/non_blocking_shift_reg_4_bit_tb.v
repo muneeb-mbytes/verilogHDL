08429f43b90a4e19447fdda1b92d82490805baf6
SUMANTH B S <126808524+sumanthbs17@users.noreply.github.com>
2023-05-16 10:09:22 +0530

    Create non_blocking_shift_reg_4_bit_tb.v

---
+++ Blocking and Non-Blocking Part-3/4 Bit Shift register using non blocking/non_blocking_shift_reg_4_bit_tb.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+module shiftreg_4bit_tb;
+
+	// Inputs
+	reg clock;
+	reg clear;
+	reg [3:0] A;
+
+	// Outputs
+	wire E;
+
+	// Instantiate the Unit Under Test (UUT)
+	shiftreg_4bit uut (
+		.clock(clock), 
+		.clear(clear), 
+		.A(A), 
+		.E(E)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clock = 0;
+		clear = 0;
+		A = 0;
+      
+		
+		 A <= #3 13;
+		 clear <= #6 1;
+		
+		
+		#80 clear <= 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+
+	end
+      always #5 clock = ~clock;
+endmodule
+
