8f79ed38a732631e9e6d3dce39b569fac4488aa7
Srivathsa2304 <103250421+Srivathsa2304@users.noreply.github.com>
2023-05-16 10:17:44 +0530

    Create TRAFFIC_LIGHT.v
    Red,Green,Yellow-Traffic Light

---
+++ MODELING FINITE STATE MACHINES/TRAFFIC_LIGHT.v
@@ -0,0 +1,23 @@
+module cyclic_lamp (clock, light);
+  input clock;
+  output reg [0:2] light;
+  parameter S0=0, S1=1, S2=2;
+  parameter RED=3'b100, GREEN=3'b010, YELLOW=3'b001;
+  reg [0:1] state;
+always @(posedge clock)
+case (state)
+  S0: begin // S0 means RED
+      light <= GREEN; state <= S1;
+      end
+  S1: begin // S1 means GREEN
+      light <= YELLOW; state <= S2;
+      end
+  S2: begin // S2 means YELLOW
+      light <= RED; state <= S0;
+      end
+  default: begin
+           light <= RED;
+           state <= S0;
+           end
+endcase
+endmodule
