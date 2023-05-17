d24637793110d055161a9a36697ff2d0fbb6d8e9
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-05-17 10:45:56 +0530

    Update MUL_test.v

--- MULTIPLICATION USING REPEATED ADDITION/MUL_test.v
+++ MULTIPLICATION USING REPEATED ADDITION/MUL_test.v
@@ -1,23 +1,23 @@
 module MUL_test;
 reg [15:0] data_in;
+wire [15:0] Y;
 reg clk, start;
 wire done;
-mul_datapath DP (eqz, LdA, LdB, LdP, clrP, decB, data_in, clk);
+mul_datapath DP (Y,eqz, LdA, LdB, LdP, clrP, decB, data_in, clk);
 controller CON (LdA, LdB, LdP, clrP, decB, done, clk, eqz, start);
 initial
 begin
 clk = 1'b0;
-#3 start = 1'b1;
+#10; start = 1'b1;
 end
 always #5 clk = ~clk;
 initial
 begin
-#17 data_in = 17;
-#10 data_in = 5;
+#10; data_in = 16'd4;
+#10; data_in = 16'd3;
 end
 initial
 begin
 $monitor ($time, " %d %b", DP.Y, done);
-$dumpfile ("mul.vcd"); $dumpvars (0, MUL_test);
 end
-endmodule 
+endmodule
