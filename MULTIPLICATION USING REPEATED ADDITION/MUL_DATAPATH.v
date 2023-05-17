7b2c5c87fd546fcba1ae0d955d26a25fd409e602
Srivathsa2304 <103250421+Srivathsa2304@users.noreply.github.com>
2023-05-17 16:12:34 +0530

    Update MUL_DATAPATH.v

--- MULTIPLICATION USING REPEATED ADDITION/MUL_DATAPATH.v
+++ MULTIPLICATION USING REPEATED ADDITION/MUL_DATAPATH.v
@@ -1,9 +1,9 @@
-module mul_datapath (Y,eqz, LdA, LdB, LdP, clrP, decB, data_in, clk);
+module mul_datapath (eqz, LdA, LdB, LdP, clrP, decB, data_in, clk);
 input LdA, LdB, LdP, clrP, decB, clk;
 input [15:0] data_in;
 output eqz;
-wire [15:0] X, Z, Bout;
-output [15:0] Y;
+wire [15:0] X,Y, Z, Bout;
+//output [15:0] Y;
 PIPO1 A (X, data_in, LdA, clk);
 PIPO2 P (Y, Z, LdP, clrP, clk);
 CNTR B (Bout, data_in, LdB, decB, clk);
