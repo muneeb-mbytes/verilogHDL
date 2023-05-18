36f51c6915af37e4078c3e8c636ea95cae43440f
Srivathsa2304 <103250421+Srivathsa2304@users.noreply.github.com>
2023-05-18 09:49:52 +0530

    Update PIPO1.v

--- MULTIPLICATION USING REPEATED ADDITION/PIPO1.v
+++ MULTIPLICATION USING REPEATED ADDITION/PIPO1.v
@@ -2,10 +2,15 @@ module PIPO1 (X, data_in,LdA, clk);
 input [15:0] data_in;
 input LdA, clk;
 output reg [15:0] X;
-always @(posedge clk)
-if (LdA)
+wire sr;
+always @(posedge clk && LdA)
+if (sr)
+  X<=16'b0;
+else 
   X <= data_in;
 endmodule 
+
+
 
 
 
