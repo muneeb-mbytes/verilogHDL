e82fb763c003a1506f852b82adf8fcce3d91b11b
Rakshitha R <109401667+RakshithaRShettigar@users.noreply.github.com>
2023-05-18 10:47:04 +0530

    Create ring_counter_correct.v

---
+++ Blocking and Non-Blocking Part-2/ring_counter_correct.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module ring_counter_correct(clk, init, count);
+ input clk, init; 
+ output reg [7:0] count; 
+ always @ (posedge clk) 
+  begin 
+   if (init)
+    count <= 8'b10000000; 
+   else begin 
+    count <= count << 1; 
+    count[0] <= count[7]; 
+   end 
+ end 
+endmodule
+
+
