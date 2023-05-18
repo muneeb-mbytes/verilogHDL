07b874f9c8d1bda08976c6d5f37d98b8bc32601c
Nikhila <108714280+Nikhilaraghuram@users.noreply.github.com>
2023-05-18 11:40:25 +0530

    Update decoder.v

--- verilog Description styles/decoder.v
+++ verilog Description styles/decoder.v
@@ -1,6 +1,8 @@
-module generate_decoder (out, in, select); 
-input in; input [0:1] select; 
-output [0:3] out; 
-assign out[select] = in; 
+module generate_decoder (out, in); 
+			input [1:0] in; 
+         output  [3:0] out; 
+			assign out[0] = (~ in[1] & ~in[0]);
+			assign out[1] = (~ in[1] & in[0]);
+			assign out[2] = ( in[1] & ~in[0]);
+			assign out[3] = ( in[1] & in[0]);
 endmodule
-
