8f50e9fb53d77b9a61b93c2631dbcca37cec0288
Myron Dcunha <67193041+Myron2812@users.noreply.github.com>
2023-05-20 14:18:58 +0530

    updated and renamed for 7 segment display functionality

--- MODELING FINITE STATE MACHINES/TRAFFIC_LIGHT(LOGIC).v
+++ MODELING FINITE STATE MACHINES/TRAFFIC_LIGHT(LOGIC).v
@@ -1,11 +1,19 @@
-module cyclic_lamp (clock, light);
-  input clock;
+module cyclic_lamp (clock, light,fast_clk);
+  input fast_clk;
   output reg [0:2] light;
   parameter S0=0, S1=1, S2=2;
   parameter RED=3'b100, GREEN=3'b010, YELLOW=3'b001;
-  reg [0:1] state;
-always @(posedge clock)
-case (state)
+  reg [1:0] state;
+  output reg clock=1'd0;
+  reg [24:0] counter=0; 
+  always@(posedge fast_clk)
+  begin
+  counter=counter+1'b1;
+  if (counter==0)
+  clock=~clock;
+  end
+  always @(posedge clock)
+  case (state)
   S0: begin // S0 means RED
         light <= GREEN; state <= S1;
       end
