3fa1753c3ecbc691eb90848b0a2f6e53b7c1651c
amansasidharan <118187955+amansasidharan@users.noreply.github.com>
2023-05-22 10:16:47 +0530

    Update fulladder.v

--- Synthesizable verilog/fulladder using task/fulladder.v
+++ Synthesizable verilog/fulladder using task/fulladder.v
@@ -1,1 +1,17 @@
+module fulladder (s, cout, a, b, cin); 
+          input a, b, cin; 
+          output reg s, cout; 
+
+          always @(a or b or cin) 
+          FA (s, cout, a, b, cin); 
+
+          task FA; 
+          output sum, carry; 
+          input A, B, C; 
+          begin 
+              #5 sum = A ^ B ^ C; 
+              carry = (A&B) | (B&C) | (C&A); 
+           end 
+         endtask
+      endmodule   
 
