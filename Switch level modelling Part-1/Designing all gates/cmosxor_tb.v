ccd7d51912369ffb940de3f65aab7c4649075db4
Rakshitha R <109401667+RakshithaRShettigar@users.noreply.github.com>
2023-05-22 20:03:01 +0530

    Create cmosxor_tb.v

---
+++ Switch level modelling Part-1/Designing all gates/cmosxor_tb.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+
+module cmosxor_tb;
+
+	// Inputs
+	reg x;
+	reg y;
+
+	// Outputs
+	wire f;
+   integer k;
+	// Instantiate the Unit Under Test (UUT)
+	cmosxor uut (
+		.x(x), 
+		.y(y), 
+		.f(f)
+	);
+
+	initial begin
+		// Initialize Inputs
+		for(k=0; k<5; k=k+1)
+		begin
+		#5 {x, y} = k;
+      $display("x: %b, y: %b, f: %b",x,y,f);
+		// Wait 100 ns for global reset to finish
+		#100;
+      end
+
+	end
+      
+endmodule
+
