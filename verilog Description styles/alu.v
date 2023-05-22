1b35c991449dcf3761e607fecefe69ceab74dd49
Srikala <105665434+srikala-reddi@users.noreply.github.com>
2023-05-22 10:29:35 +0530

    alu.v

--- verilog Description styles/alu.v
+++ verilog Description styles/alu.v
@@ -2,11 +2,11 @@ module alu_16bit(x,y,z,sign,zero,carry,parity,overflow
     );
 	 input [15:0] x,y;
 	 output [15:0]z;
-	 output sign,zero,carry,parity,overflow;
+	 output sign, zero,carry, parity, overflow;
 	 
 assign {carry,z}=x+y;//  16 bit add
 assign sign=z [15];
 assign zero=~|z;
 assign parity =~^z;
 assign overflow=(x[15] &y[15] &~z[15])| (~x[15] &y[15] &z[15]);
-endmodule
+end module
