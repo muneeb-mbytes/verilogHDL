1ccc4d8aa1ffbe5e7d0ba6bc51d58f7147d06e24
Rakshitha R <109401667+RakshithaRShettigar@users.noreply.github.com>
2023-05-22 19:57:48 +0530

    Create cmosnand_tb.v

---
+++ Switch level modelling Part-1/Designing all gates/cmosnand_tb.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+
+module cmosnand_tb;
+
+	// Inputs
+	reg x;
+	reg y;
+
+	// Outputs
+	wire f;
+   integer k;
+	// Instantiate the Unit Under Test (UUT)
+	cmosnand uut (
+		.x(x), 
+		.y(y), 
+		.f(f)
+	);
+
+	initial begin
+		// Initialize Inputs
+		for(k=0; k<4; k=k+1)
+		begin
+		#5 {x, y} = k;
+      $display("x: %b, y: %b, f: %b",x,y,f);
+		// Wait 100 ns for global reset to finish
+		#100;
+      end
+	end
+      
+endmodule
+
