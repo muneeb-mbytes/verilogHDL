a1820438f59aeb0103f8dfb17324c32c25bb6c48
Srivathsa2304 <103250421+Srivathsa2304@users.noreply.github.com>
2023-05-23 10:19:37 +0530

    Update SEQUENCE_DETECTOR.v

--- MODELING FINITE STATE MACHINES/SEQUENCE_DETECTOR.v
+++ MODELING FINITE STATE MACHINES/SEQUENCE_DETECTOR.v
@@ -1,43 +1,40 @@
 module fsm_seq_detector(
     input clk,
     input rst,
-    input [9:0]data,
-    input x,
-    output reg z );
+    input [9:0] x,
+	 output reg buzz,
+    output reg z
+    );
 parameter A=0,B=1,C=2,D=3;
 reg [1:0] PS,NS;
-assign data=10'b0001111010;
 always@(posedge clk or posedge rst)
-	begin
-		if(rst) 
-			begin
-				PS<=A;
-			end
-		else 
-			begin
-				PS<=NS;
-			end
+begin
+if(rst) begin
+		PS<=A;
+		end
+else begin
+	PS<=NS;
 	end
+end
 always@(PS,x)
 	begin
-		case(PS)
-			A:begin
-				z<=x?0:0;
-				NS<=x?B:A;
-			  end
-			B:begin
-				z<=x?0:0;
-				NS<=x?C:A;
-			  end
-			C:begin
-				z<=x?1:0;
-				NS<=x?D:A;
-			  end
-			D:begin
-				z<=x?1:0;
-				NS<=x?D:A;
-		
-			  end
-		endcase
-	end
+	case(PS)
+		A:begin
+			z<=x?0:0;
+			NS<=x?B:A;
+		  end
+		B:begin
+			z<=x?0:0;
+			NS<=x?C:A;
+		  end
+		C:begin
+			z<=x?1:0;
+		   NS<=x?D:A;
+		  end
+		D:begin
+			z<=x?1:0;
+			NS<=x?D:A;
+		  end
+	endcase
+end
 endmodule
